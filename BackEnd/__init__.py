b7fe50816c8a4b5a9f721725bf84ba6cec3a0d36
Bryan Hernandez <106711408+hBrymiri@users.noreply.github.com>
2026-06-10 21:53:54 -0700

    Delete BackEnd directory

--- BackEnd/__init__.py
+++
@@ -1,6 +0,0 @@
-# BackEnd/__init__.py
-
-from .face_reg import Recognizer
-from .database import insert_embedding, fetch_all_embeddings
-
-__all__ = ["Recognizer", "insert_embedding", "fetch_all_embeddings"]
